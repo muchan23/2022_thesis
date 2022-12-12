30accd028628df54978443cf4dbaaf407c1b3a77
Yuki <murakami-yuki866@g.ecc.u-tokyo.ac.jp>
2022-12-12 10:10:49 +0900

    init

---
+++ .anaconda/navigator/scripts/notebook.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+. /Users/murakamiyuki/opt/anaconda3/bin/activate /Users/murakamiyuki/opt/anaconda3
+open /Users/murakamiyuki/opt/anaconda3/bin/jupyter_mac.command >/dev/null 2>/dev/null
---
+++ .anaconda/navigator/scripts/spyder.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+source //anaconda3/bin/activate //anaconda3
+spyder >/Users/murakamiyuki/.anaconda/navigator/scripts/spyder-out-1.txt 2>/Users/murakamiyuki/.anaconda/navigator/scripts/spyder-err-1.txt
