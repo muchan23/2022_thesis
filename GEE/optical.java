ef20f472bd37bf60c31fd2d0e70dd9abb854488c
muchan23 <55784567+muchan23@users.noreply.github.com>
2022-12-25 11:40:58 +0900

    Create optical.java

---
+++ GEE/optical.java
@@ -0,0 +1,31 @@
+var geometry = ee.Geometry.Rectangle([130.69643796118186,32.16909272727556, 130.79943478735373,32.258558460819906]);
+ 
+//geometryが確認できる位置まで地図を移動する
+Map.centerObject(geometry)
+ 
+//バンド値を元に画像をつトルゥーカラーとしてレンダリングするための設定
+var trueColor = {
+  bands:['B4', 'B3', 'B2'],
+  min: 0.0,
+  max: 0.4,
+};
+ 
+//イメージコレクションの中から対象となるイメージを取得
+var imageObject = ee.ImageCollection('COPERNICUS/S1_GRD')   //画像を指定
+                  .filterDate('2020-06-01', '2020-08-31') //期間でフィルタリング
+                  .filterMetadata('CLOUD_COVER', 'less_than', 2) //なるべく雲の少ない写真を選ぶ
+                  .filterBounds(geometry) //gemetryのエリアが含まれる写真を選択
+                  .first() //絞り込んだコレクションの中から最初の１枚を取得
+                  .visualize(trueColor) //レンダリング設定を渡す
+ 
+                  
+//地図上に衛星画像をオーバーレイレイする
+Map.addLayer(imageObject ,null, 'True Color');
+ 
+//イメージをGoogleドライブに出力する
+Export.image.toDrive({
+  image: imageObject,
+  description: 'sarimage',
+  scale: 30,
+  region: geometry
+});
